dc1b5837e39f9a33d3063b471b68a746cced2cee
nguyenvandiatlvn3@gmail.com <nguyenvandiatlvn3@gmail.com>
2024-07-18 09:01:20 +0700

    feat: get token success

--- PushNotificationFirebaseDemo/App.xaml.cs
+++ PushNotificationFirebaseDemo/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace PushNotificationFirebaseDemo
+﻿using Plugin.FirebasePushNotification;
+
+namespace PushNotificationFirebaseDemo
 {
     public partial class App : Application
     {
@@ -7,6 +9,29 @@
             InitializeComponent();
 
             MainPage = new AppShell();
+            //App chạy lần đầu tiên sau khi cài. IOS khong chay duoc tren may ao. Chi chay duoc tren may that
+            CrossFirebasePushNotification.Current.OnTokenRefresh += (s, p) =>
+            {
+                String a = p.Token;
+                System.Diagnostics.Debug.WriteLine($"TOKEN : {p.Token}");
+
+            };
+            // Push message received event
+            CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
+            {
+                //System.Diagnostics.Debug.WriteLine("Received");
+                //Plugin.Badge.CrossBadge.Current.SetBadge(1); //Nhieu thiet bi android ko hien thi duoc
+            };
+            //Push message received event
+            CrossFirebasePushNotification.Current.OnNotificationOpened += (s, p) =>
+            {
+                //Plugin.Badge.CrossBadge.Current.ClearBadge();
+                //System.Diagnostics.Debug.WriteLine("Opened");
+                //foreach (var data in p.Data)
+                //{
+                //    System.Diagnostics.Debug.WriteLine($"{data.Key} : {data.Value}");
+                //}
+            };
         }
     }
 }
